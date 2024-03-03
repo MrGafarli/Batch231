b10e2f50c9808450fcf822f8ea9913127562cfb7
MrGafarli <111965076+MrGafarli@users.noreply.github.com>
2024-03-03 12:38:10 +0400

    Update Ders1.java

--- src/main/java/d8stringmanipulations/Ders1.java
+++ src/main/java/d8stringmanipulations/Ders1.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Ders1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String gunismi = input.next();
-        if (gunismi.equalsIgnoreCase("Saturday") || gunismi.equalsIgnoreCase("Sunday")) {
+        String dayName = input.next();
+        if (dayName.equalsIgnoreCase("Saturday") || dayName.equalsIgnoreCase("Sunday")) {
             System.out.println("Weekend");
-        } else if (gunismi.equalsIgnoreCase("Monday")
-                || gunismi.equalsIgnoreCase("Tuesday")
-                || gunismi.equalsIgnoreCase("Wednesday")
-                || gunismi.equalsIgnoreCase("Thursday")
-                || gunismi.equalsIgnoreCase("Friday"))
+        } else if (dayName.equalsIgnoreCase("Monday")
+                || dayName.equalsIgnoreCase("Tuesday")
+                || dayName.equalsIgnoreCase("Wednesday")
+                || dayName.equalsIgnoreCase("Thursday")
+                || dayName.equalsIgnoreCase("Friday"))
         {
             System.out.println("Weekday");
         }else {
